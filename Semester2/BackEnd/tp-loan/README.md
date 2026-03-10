435df3ab871c8846346556b3621dbf95db757f7f
tiouli-salma <tiouli.salma@ensam-casa.ma>
2026-03-10 19:55:11 +0000

    Remove duplicate tp-loan folder

--- Semester2/BackEnd/tp-loan/README.md
+++
@@ -1,1 +0,0 @@
-# ENSAM-AI-CS
--- Semester2/BackEnd/tp-loan/routes/api.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\LoanController;
-
-Route::patch('loans/{id}/return', [LoanController::class, 'return']);
-Route::apiResource('loans', LoanController::class);
